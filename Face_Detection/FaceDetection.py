145c5ba470a9cf62616694a3f4af7529adcd4b1a
Daniel Kashkett <Dkashkett@gmail.com>
2021-04-30 10:26:22 -0600

    refactor FaceDetector

--- Face_Detection/FaceDetection.py
+++ Face_Detection/FaceDetection.py
@@ -1,54 +1,42 @@
 import dlib
 import os
+import cv2
 
 
-class FaceDetector:
-    CWD = os.path.abspath(os.path.dirname(__file__))
-    MODEL_PATH = CWD.replace(
-        os.path.dirname(__file__), "models/shape_predictor_68_face_landmarks.dat"
-    )
-    # dlib detectors
-    FACE = dlib.get_frontal_face_detector()
-    LANDMARKS = dlib.shape_predictor(MODEL_PATH)
+class FaceDetector(object):
+    CWD = os.getcwd()
+    MODEL_PATH = CWD + "/models/shape_predictor_68_face_landmarks.dat"
 
     def __init__(self):
-        pass
+        self._face_detector = dlib.get_frontal_face_detector()
+        self._landmark_detector = dlib.shape_predictor(self.MODEL_PATH)
+        self.eye_right = None
+        self.eye_left = None
+        self.face_box = None
 
     # method for detecting faces and returning coordinates
     def detect(self, frame):
-        face_results = self.FACE(frame)
+        _results = self._face_detector(frame)
+        faceDetected = len(_results) == 1
         # if there is only one face present
-        if len(face_results) == 1:
+        if faceDetected:
             # box is the dlib rectangle object for each face
-            box = face_results[0]
+            box = _results[0]
             # these are the individual coordinates of the face bounding box
-            x1, y1, x2, y2 = box.left(), box.top(), box.right(), box.bottom()
+            self.face_box = [box.top(), box.bottom(), box.left(), box.right()]
             # dlib landmarks object
-            landmark_results = self.LANDMARKS(image=frame, box=box)
-            # building a dictionary with arrays of tuple coordinate pairs (x,y) as values
-            # and face landmarks as keys
-            results = {}
-            results["face_box"] = [(x1, y1), (x2, y2)]
-            results["left_eye"] = [
-                (landmark_results.part(i).x, landmark_results.part(i).y)
-                for i in range(36, 42)
+            landmarks = self._landmark_detector(image=frame, box=box)
+            self.eye_left = [
+                (landmarks.part(i).x, landmarks.part(i).y) for i in range(36, 42)
             ]
-            results["left_eyebrow"] = [
-                (landmark_results.part(i).x, landmark_results.part(i).y)
-                for i in range(17, 22)
+            self.eye_right = [
+                (landmarks.part(i).x, landmarks.part(i).y) for i in range(42, 48)
             ]
-            results["right_eye"] = [
-                (landmark_results.part(i).x, landmark_results.part(i).y)
-                for i in range(42, 48)
-            ]
-            results["right_eyebrow"] = [
-                (landmark_results.part(i).x, landmark_results.part(i).y)
-                for i in range(22, 27)
-            ]
-            return results
+            return True
         else:
-            return None
+            return False
 
 
 if __name__ == "__main__":
+
     pass
